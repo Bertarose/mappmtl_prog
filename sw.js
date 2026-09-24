eb26b49ff1ad6fde99682491c554ca551c2d8c29
Kim Maurice <100428390+Bertarose@users.noreply.github.com>
2026-09-24 17:48:23 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 // ══════════════════════════════════════════════════════════
 //  MAPP_MTL 2026 — Service Worker  (Network First)
 // ══════════════════════════════════════════════════════════
-const CACHE = 'mapp-2026-v10';
+const CACHE = 'mapp-2026-v11';
 const ASSETS = [
   '/',
   '/index.html',
